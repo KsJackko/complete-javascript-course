c0a25c212c582a0499d256234730ab4940fead54
KsJackko <jackkozhang@gmail.com>
2025-06-14 18:36:08 +0800

    #85 Handling an Esc Keyporess Event

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -9,28 +9,60 @@
 
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
-let score = 20;
 
-document.querySelector('.check').addEventListener('click', function () {
-  const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
-  // console.log(typeof document.querySelector('.score').textContent);
-  if (!guess) {
-    document.querySelector('.message').textContent = 'No Number!';
-  } else if (score <= 1) {
-    document.querySelector('.message').textContent = 'You lost the game!';
-    document.querySelector('.score').textContent = score - 1;
-  } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = 'Correct Number!';
-  } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = 'Too high!';
-    score--;
-    document.querySelector('.score').textContent = score;
-  } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = 'Too low!';
-    score--;
-    document.querySelector('.score').textContent = score;
-  }
-});
+//79. Manipulating CSS Styles
+// let secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+// let score = 20;
+// let highestScore = 0;
+
+// const displayMessage = function (message) {
+//   document.querySelector('.message').textContent = message;
+// };
+
+// document.querySelector('.check').addEventListener('click', function () {
+//   const guess = Number(document.querySelector('.guess').value);
+//   console.log(guess, typeof guess);
+//   // console.log(typeof document.querySelector('.score').textContent);
+//   //when there is no input
+//   if (!guess) {
+//     // document.querySelector('.message').textContent = 'No Number!';
+//     displayMessage('No Number!');
+//     //when scores are run out
+//   } else if (score <= 1) {
+//     //document.querySelector('.message').textContent = 'You lost the game!';
+//     displayMessage('You lost the game!');
+//     document.querySelector('.score').textContent = 0;
+
+//     //when player wins
+//   } else if (guess === secretNumber) {
+//     //document.querySelector('.message').textContent = 'Correct Number!';
+//     displayMessage('Correct Number!');
+//     document.querySelector('.number').textContent = secretNumber;
+//     // Change the background color when win
+//     document.querySelector('body').style.backgroundColor = '#60b347';
+//     document.querySelector('.number').style.width = '30rem';
+
+//     if (highestScore < score)
+//       document.querySelector('.highscore').textContent = score;
+
+//     //when guess is too high
+//   } else if (guess !== secretNumber) {
+//     guess > secretNumber
+//       ? displayMessage('Too high!')
+//       : displayMessage('Too low!');
+//     score--;
+//     document.querySelector('.score').textContent = score;
+//   }
+// });
+
+// document.querySelector('.again').addEventListener('click', function () {
+//   secretNumber = Math.trunc(Math.random() * 20) + 1;
+//   score = 20;
+//   document.querySelector('.score').textContent = 20;
+//   displayMessage('Start guessing...');
+//   document.querySelector('.number').textContent = '?';
+//   document.querySelector('body').style.backgroundColor = '#222';
+//   document.querySelector('.number').style.width = '15rem';
+//   document.querySelector('.guess').value = '';
+// });

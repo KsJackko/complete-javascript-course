8938a05b7ff8c6da2e9b6579607b0c1bc1b5e3e3
KsJackko <jackkozhang@gmail.com>
2025-06-21 12:46:24 +0800

    #154 creating dom elements

--- complete-javascript-course-master/10-Functions/starter/script.js
+++ complete-javascript-course-master/10-Functions/starter/script.js
@@ -17,115 +17,290 @@
 // createBooking('PHI1209', undefined, 88);
 
 //#138 fucntions accept callback functions
-const oneWord = function (str) {
-  return str.replace(/ /g, '').toLowerCase();
-};
-const upperFirstWord = function (str) {
-  const [first, ...other] = str.split(' ');
-  return [first.toUpperCase(), ...other].join(' ');
-};
-console.log(oneWord('Jackko'));
-console.log(upperFirstWord('i love u'));
+// const oneWord = function (str) {
+//   return str.replace(/ /g, '').toLowerCase();
+// };
+// const upperFirstWord = function (str) {
+//   const [first, ...other] = str.split(' ');
+//   return [first.toUpperCase(), ...other].join(' ');
+// };
+// console.log(oneWord('Jackko'));
+// console.log(upperFirstWord('i love u'));
 
-// Higher-order function
-const transformer = function (str, fn) {
-  console.log(`Original str: ${str}`);
-  console.log(`Transformed string: ${fn(str)}`);
-  console.log(`Transformed by: ${fn.name}`);
-};
+// // Higher-order function
+// const transformer = function (str, fn) {
+//   console.log(`Original str: ${str}`);
+//   console.log(`Transformed string: ${fn(str)}`);
+//   console.log(`Transformed by: ${fn.name}`);
+// };
 
-transformer('JavaScript is the best!', upperFirstWord);
+// transformer('JavaScript is the best!', upperFirstWord);
 
-// JS use callbacks all the time
-const high5 = function () {
-  console.log('Hi');
-};
+// // JS use callbacks all the time
+// const high5 = function () {
+//   console.log('Hi');
+// };
 
-document.body.addEventListener('click', high5);
+// document.body.addEventListener('click', high5);
 
-['Jackko', 'Jackko2', 'Jackko3'].forEach(high5);
+// ['Jackko', 'Jackko2', 'Jackko3'].forEach(high5);
 
-//#139 Functions Returning Functions
-// const greet = function (greeting) {
-//   return function (name) {
-//     console.log(`${greeting}, ${name}`);
+// //#139 Functions Returning Functions
+// // const greet = function (greeting) {
+// //   return function (name) {
+// //     console.log(`${greeting}, ${name}`);
+// //   };
+// // };
+
+// const greet = greeting => name => console.log(`${greeting}, ${name}`);
+
+// const greeterHey = greet('Hey');
+// greeterHey('Jackko');
+
+// greet('Hello')('Jackko');
+
+// //#140 The call and apply method
+// const cathay = {
+//   airline: 'Cathay',
+//   iatacode: 'HKG',
+//   booking: [],
+//   book(flightNum, name) {
+//     console.log(
+//       `${name} books a seat on ${this.airline} flight ${flightNum} ${this.iatacode}`
+//     );
+//     this.booking.push({
+//       flight: `${this.iatacode}${flightNum}`,
+//       name,
+//     });
+//   },
+// };
+// cathay.book('0987654321', 'Jackko');
+// console.log(cathay);
+
+// const eurowings = {
+//   airline: 'Eurowings',
+//   iatacode: 'EW',
+//   booking: [],
+// };
+
+// const book = cathay.book;
+// // does not work
+// // book(234, 'Jacky');
+// book.call(eurowings, 234, 'Sunny Lai');
+// console.log(eurowings);
+
+// book.call(cathay, 456, 'Andrew');
+// console.log(cathay);
+
+// const swiss = {
+//   airline: 'Swiss',
+//   iatacode: 'LX',
+//   booking: [],
+// };
+// book.call(swiss, 456, 'Andrew');
+
+// // Apply method
+// const flightData = [596, 'Jenny Li'];
+// book.apply(swiss, flightData);
+// console.log(swiss);
+
+// book.call(swiss, ...flightData);
+
+// //#141 The call and apply method
+// // bind method
+// // book.call(eurowings, 234, 'Sunny Lai');
+
+// const bookEW = book.bind(eurowings);
+// const bookHKG = book.bind(cathay);
+// const bookLX = book.bind(swiss);
+
+// bookEW(23, 'Peter Wong');
+// console.log(eurowings);
+
+// const bookEW23 = book.bind(eurowings, 23);
+// bookEW23('Nokk Tsuang');
+
+// //with Event Listeners
+// cathay.planes = 300;
+// cathay.buyPlane = function () {
+//   console.log(this);
+//   this.planes++;
+//   console.log(this.planes);
+// };
+
+// document
+//   .querySelector('.buy')
+//   .addEventListener('click', cathay.buyPlane.bind(cathay));
+
+// // Partial application
+// const addTax = (rate, value) => value + value * rate;
+// console.log(addTax(0.1, 200));
+
+// const addVAT = addTax.bind(null, 0.23);
+// console.log(addVAT(100));
+// // const addTax = (value) => value+value*0.23;
+
+// const addTaxRate = function (rate) {
+//   return function (value) {
+//     return value + value * rate;
 //   };
 // };
 
-const greet = greeting => name => console.log(`${greeting}, ${name}`);
+// const addVAT = addTaxRate(0.23);
 
-const greeterHey = greet('Hey');
-greeterHey('Jackko');
+// console.log(addVAT(200));
 
-greet('Hello')('Jackko');
+// Coding Challenge #1
 
-//#140 The call and apply method
-const cathay = {
-  airline: 'Cathay',
-  iatacode: 'HKG',
-  booking: [],
-  book(flightNum, name) {
-    console.log(
-      `${name} books a seat on ${this.airline} flight ${flightNum} ${this.iatacode}`
-    );
-    this.booking.push({
-      flight: `${this.iatacode}${flightNum}`,
-      name,
-    });
-  },
-};
-cathay.book('0987654321', 'Jackko');
-console.log(cathay);
+/* 
+Let's build a simple poll app!
 
-const eurowings = {
-  airline: 'Eurowings',
-  iatacode: 'EW',
-  booking: [],
-};
+A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter object below.
 
-const book = cathay.book;
-// does not work
-// book(234, 'Jacky');
-book.call(eurowings, 234, 'Sunny Lai');
-console.log(eurowings);
+Here are your tasks:
 
-book.call(cathay, 456, 'Andrew');
-console.log(cathay);
+1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
+  1.1. Display a prompt window for the user to input the number of the selected option. The prompt should look like this:
+        What is your favourite programming language?
+        0: JavaScript
+        1: Python
+        2: Rust
+        3: C++
+        (Write option number)
+  
+  1.2. Based on the input number, update the answers array. For example, if the option is 3, increase the value AT POSITION 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g answer 52 wouldn't make sense, right?)
+2. Call this method whenever the user clicks the "Answer poll" button.
+3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1". 
+4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
 
-const swiss = {
-  airline: 'Swiss',
-  iatacode: 'LX',
-  booking: [],
-};
-book.call(swiss, 456, 'Andrew');
+HINT: Use many of the tools you learned about in this and the last section 😉
 
-// Apply method
-const flightData = [596, 'Jenny Li'];
-book.apply(swiss, flightData);
-console.log(swiss);
+BONUS: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do NOT put the arrays in the poll object! So what shoud the this keyword look like in this situation?
 
-book.call(swiss, ...flightData);
+BONUS TEST DATA 1: [5, 2, 3]
+BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
 
-//#141 The call and apply method
-// bind method
-// book.call(eurowings, 234, 'Sunny Lai');
+GOOD LUCK 😀
+*/
 
-const bookEW = book.bind(eurowings);
-const bookHKG = book.bind(cathay);
-const bookLX = book.bind(swiss);
+// const poll = {
+//   question: 'What is your favourite programming language?',
+//   options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+//   // This generates [0, 0, 0, 0]. More in the next section 😃
+//   answers: new Array(4).fill(0),
+//   registerNewAnswer() {
+//     this.options.unshift(this.question);
+//     const str = this.options.join('\n');
+//     //Display a prompt window for the user
+//     const choice = Number(prompt(str));
+//     // Based on the input number, update the answers array.
+//     typeof choice === 'number' && choice >= 0 && choice < this.answers.length
+//       ? this.answers[choice]++
+//       : console.log('Invalid input !');
+//     this.displayResult();
+//     this.displayResult('string');
+//   },
+//   displayResult(type = 'array') {
+//     type === 'array'
+//       ? console.log(`[${this.answers.join(', ')}]`)
+//       : console.log(`Poll results are ${this.answers.join(', ')}`);
+//   },
+// };
 
-bookEW(23, 'Peter Wong');
-console.log(eurowings);
+// document
+//   .querySelector('.poll')
+//   .addEventListener('click', poll.registerNewAnswer.bind(poll));
 
-const bookEW23 = book.bind(eurowings, 23);
-bookEW23('Nokk Tsuang');
+// poll.displayResult.call({ answers: [5, 2, 3] });
+// poll.displayResult.call({ answers: [5, 2, 3] }, 'string');
 
-//with Event Listeners
-cathay.planes = 300;
-cathay.buyPlane = function () {
-  console.log(this);
-  this.planes++;
-  console.log(this.planes);
-};
+// poll.displayResult.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
 
-document.querySelector('.buy').addEventListener('click', cathay.buyPlane);
+// Immediately invoked function expressions
+
+// const runOnce = function () {
+//   console.log('This will never run again');
+// };
+// runOnce();
+
+// // IIFE
+// (function () {
+//   console.log('This will never run again');
+//   const isPrivate = 23;
+// })();
+
+// (() => console.log('This will never run again'))();
+
+// closures
+// const secureBooking = function () {
+//   let passengerCount = 0;
+//   return function () {
+//     passengerCount++;
+//     console.log(`${passengerCount} passengers`);
+//   };
+// };
+
+// const booker = secureBooking();
+
+// booker();
+// booker();
+// booker();
+
+// console.dir(booker);
+
+// let f;
+// const g = function () {
+//   const a = 23;
+//   f = function () {
+//     console.log(a * 2);
+//   };
+// };
+
+// const h = function () {
+//   const b = 999;
+//   f = function () {
+//     console.log(b * 2);
+//   };
+// };
+
+// g();
+// f();
+// console.log(f);
+
+// h();
+// f();
+// console.log(h);
+
+// const boardPassengers = function (n, wait) {
+//   const perGroup = n / 3;
+//   setTimeout(function () {
+//     console.log(`We are now boarding all ${n} passengers`);
+//     console.log(
+//       `There are three group, each group include ${perGroup} passengers`
+//     );
+//   }, 1000);
+//   console.log(`Will start boarding in ${wait} seconds`);
+// };
+
+// const perGroup = 1000;
+// boardPassengers(1800, 3);
+
+// Coding Challenge #2
+
+/* 
+This is more of a thinking challenge than a coding challenge 🤓
+
+Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+
+And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
+
+GOOD LUCK 😀
+*/
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
--- complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -61,16 +61,136 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovement = function (movements) {
+  containerMovements.innerHTML = '';
+
+  movements.forEach((mov, i) => {
+    const type = mov < 0 ? 'withdrawal' : 'deposit';
+    const html = `
+       <div class="movements__row">
+          <div class="movements__type movements__type--${type}">${
+      i + 3
+    }${type}</div>
+          <div class="movements__value">${mov}€</div>
+       </div>`;
+    containerMovements.insertAdjacentHTML('beforeEnd', html);
+  });
+};
+
+displayMovement(account1.movements);
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+
+// //SLICE
+// console.log(arr.slice(2));
+// console.log(arr.slice(2, 4));
+// console.log(arr.slice(-2));
+// console.log(arr.slice(-1));
+// console.log(arr.slice());
+// console.log([...arr]);
+
+// //SPLICE
+// let myName = ['j', 'a', 'c', 'k', 'k', 'o', '-'];
+// console.log(myName.splice(-1));
+// console.log(myName);
+// myName.splice(3, 4);
+// console.log(myName);
+
+// //REVERSE
+// let myName2 = ['j', 'a', 'c', 'k', 'k', 'o'];
+// console.log(myName2.reverse());
+// console.log(myName2);
+
+// //CONCAT
+// let lastName = ['Z', 'H', 'A', 'N', 'G'];
+// const name = myName2.concat(lastName);
+// console.log(name);
+
+// //JOIN
+// console.log(name.join('-'));
+
+// // AT Method
+// console.log(lastName.at(0));
+// console.log(lastName[0]);
+
+// console.log(lastName.at(-1));
+// console.log(lastName[lastName.length - 1]);
+
+// //Looping Array: forEach
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// //for (const movement of movements)
+// for (const [i, movement] of movements.entries()) {
+//   if (movement > 0) {
+//     console.log(`Movement${i}, You deposited ${movement}`);
+//   } else {
+//     console.log(`Movement${i}, You withdraw ${Math.abs(movement)}`);
+//   }
+// }
+
+// // Can't use break and continuse
+// movements.forEach(function (movement, i, movements) {
+//   if (movement > 0) {
+//     console.log(`Movement${i}, You deposited ${movement}`);
+//   } else {
+//     console.log(`Movement${i}, You withdraw ${Math.abs(movement)}`);
+//   }
+// });
+
+// // forEach for Map and Set
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
+
+// currencies.forEach(function (value, key, map) {
+//   console.log(`${key}: ${value}`);
+// });
+
+// // Set
+// const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+// console.log(currenciesUnique);
+// currenciesUnique.forEach(function (value, _, map) {
+//   console.log(`${value}: ${value}`);
+// });
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an adult or a puppy. A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
+
+Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
+
+1. Julia found out that the owners of the FIRST and the LAST TWO dogs actually have cats, not dogs! So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters)
+2. Create an array with both Julia's (corrected) and Kate's data
+3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 🐶")
+4. Run the function for both test datasets
+
+HINT: Use tools from all lectures in this section so far 😉
+
+TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+const checkDogs = function (dogsJulia, dogsKate) {
+  const output = function (dog, i) {
+    const type = dog >= 3 ? 'adult' : 'puppy';
+    type === 'adult'
+      ? console.log(`Dog number ${i + 1} is an adult, and is ${dog} years old`)
+      : console.log(`Dog number ${i + 1} is still a puppy 🐶`);
+  };
+  const newdogsJulia = dogsJulia.slice(1, -2);
+  newdogsJulia.forEach(output);
+  console.log('---');
+  dogsKate.forEach(output);
+};
+checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);

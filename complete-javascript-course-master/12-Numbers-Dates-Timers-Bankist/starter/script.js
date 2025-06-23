d454c85f15ebcff76749787e959d05030c1990c6
KaSing <160307874+KsJackko@users.noreply.github.com>
2025-06-23 23:46:18 +0800

    187. Adding Dates to "Bankist" App

--- complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -155,6 +155,20 @@ const updateUI = function (acc) {
 // Event handlers
 let currentAccount;
 
+// FAKE ALWAYS LOGGED IN
+currentAccount = account1;
+updateUI(currentAccount);
+containerApp.style.opacity = 1;
+
+const now = new Date();
+const day = `${now.getDate()}`.padStart(2, 0);
+const month = `${now.getMonth() + 1}`.padStart(2, 0);
+const year = now.getFullYear();
+const hour = now.getHours();
+const min = now.getMinutes();
+
+labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`;
+
 btnLogin.addEventListener('click', function (e) {
   // Prevent form from submitting
   e.preventDefault();
@@ -385,10 +399,10 @@ console.log(new Date(3 * 24 * 60 * 60 * 1000));
 */
 
 //Working with dates
+/*
 const future = new Date(2037, 10, 19, 15, 23);
 console.log(future);
 console.log(future.getFullYear());
-console.log(future.getMonth());
 console.log(future.getMonth());
 console.log(future.getDate());
 console.log(future.getDay());
@@ -404,3 +418,4 @@ console.log(Date.now());
 
 future.setFullYear(2040);
 console.log(future);
+*/

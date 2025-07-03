b88a55388f1555a27ec8e4c1961561587281a347
ZKS456 <jackko.zhang@ha.org.hk>
2025-07-03 17:05:28 +0800

    295. Let's Fix Some Bad Code: Part 2

--- complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
+++ complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -16,3 +16,80 @@ add('pizza', 2);
 add('apples', 5);
 
 console.log(cart);
+
+// console.log('Start fetching');
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log(data);
+// console.log('Something');
+
+/*
+const getLastPost = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+  console.log(data);
+
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+const lastPost = getLastPost();
+
+// Not very clean
+lastPost.then(res => console.log(res));
+
+console.log(await lastPost);
+*/
+
+const shoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} added to cart`);
+  };
+
+  const orderStock = function (product, quantity) {
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+shoppingCart2.addToCart('apple', 4);
+shoppingCart2.addToCart('pizza', 2);
+console.log(shoppingCart2);
+console.log(shoppingCart2.shippingCost);
+
+// import the external library
+// import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+import cloneDeep from 'lodash-es';
+
+const state = {
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 5 },
+  ],
+  user: { loggedIn: true },
+};
+
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+state.user.loggedIn = false;
+console.log(stateClone);
+console.log(stateDeepClone);
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+console.log(cart.find(el => el.quantity >= 2));
+
+import 'core-js';
--- complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
+++ complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
@@ -1,5 +1,13 @@
 // Exporting module
+
 console.log('Exporting module');
+
+//Blocking code
+/*
+console.log('Start blocking');
+await fetch('https://jsonplaceholder.typicode.com/posts');
+console.log('Finish fetching users');
+*/
 
 const shippingCost = 10;
 export const cart = [];
@@ -18,3 +26,5 @@ export default function (product, quantity) {
   cart.push({ product, quantity });
   console.log(`${quantity} ${product} added to cart`);
 }
+
+console.log(cart);
